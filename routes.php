374f09f8477cfad11ea35258700e2eaf91f0d6bb
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-11-27 13:29:37 +0100

    Added PHPRouter

---
+++ routes.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__.'/router.php';
+
+// ##################################################
+// ##################################################
+// ##################################################
+
+// Static GET
+// In the URL -> http://localhost
+// The output -> Index
+get('/',                            'views/index.php');
+// get('/10-28/item/$id',                    'views/item');
+// get('/10-28/$gender/shoes/$brand/$size',  'views/product');
+// get('/10-28/test/$word', function($word){
+//   out($word);
+// });
+
+// APIS
+// post('/item', 'apis/create_item');
+
+
+
+// For GET or POST
+// The 404.php which is inside the views folder will be called
+// The 404.php has access to $_GET and $_POST
+any('/404','views/404.php');
