741dd17710c005b7de6502e35625cd5c19109c2c
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-02 11:52:17 +0100

    Created primitive login view

--- views/index.php
+++ views/index.php
@@ -1,3 +1,32 @@
 <?php
 
-echo 'Hello PHPRouter';
+$title = 'Home';
+include_once __DIR__ . '/templates/header.php';
+?>
+
+<h1>Anarkist</h1>
+
+<section id="login_modal">
+    <button onclick="">X</button>
+    <form method="POST" action="/login">
+        <div>
+            <label for="email">E-mail</label>
+            <input type="email" id="email" name="email" required pattern='^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))' />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <!-- INSERT PASSWORD PATTERN -->
+            <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit" onclick="formValidation(postSession)">Log in</button>
+    </form>
+</section>
+
+<?php 
+    if (isset($_SESSION)) {
+        echo json_encode($_SESSION);
+    }
+?>
+
+<?php 
+include_once __DIR__ . '/templates/footer.php';
