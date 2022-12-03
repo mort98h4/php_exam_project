b5e518d835d327411b21e835bac73e25e5d74614
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-03 11:08:14 +0100

    Implemented redirect function

--- utils.php
+++ utils.php
@@ -1,9 +1,14 @@
 <?php
 
-function _respond($message='', $status=200) {
+function _respond(string $message='', int $status=200) {
     http_response_code($status);
     header('Content-Type: application/json');
     $res = is_array($message) ? $message : ['info' => $message];
     echo json_encode($res);
+    exit();
+}
+
+function _redirect(string $url='/') {
+    header("Location: $url");
     exit();
 }
