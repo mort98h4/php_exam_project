657ecd54c3d2e363c4a014c749a32a91ec55e5cf
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-03 11:09:11 +0100

    Created logout functionality

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -35,3 +35,18 @@ async function postSession(form) {
 
     window.location.href="/";
 }
+
+async function deleteSession() {
+    const userId = event.target.dataset.id;
+    console.log(userId);
+    const response = await fetch(`/logout/${userId}`, {
+        method: 'DELETE',
+    });
+    if (response.status !== 204) {
+        const error = await response.json();
+        console.log(error);
+        return;
+    }
+
+    window.location.href='/';
+}
