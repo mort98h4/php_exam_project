9020adb73fb6758501eae737763dc777b536afeb
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-03 11:37:28 +0100

    Implemented tailwind

---
+++ static/css/tailwindcss/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["../../../views/*.php", "../../../views/templates/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
