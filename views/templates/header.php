7683cc8f5d6243e2bc6d007cc7fcb6b3053f7f58
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-06 15:03:09 +0100

    added fontawesome

--- views/templates/header.php
+++ views/templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? out($title) . ' | Anarkist' : 'Anarkist'; ?></title>
     <link rel="stylesheet" href="./static/css/app.css" />
+    <script src="https://kit.fontawesome.com/4612253046.js" crossorigin="anonymous"></script>
     <script src="./static/scripts/main.js" defer></script>
 </head>
 <body class="bg-black">
