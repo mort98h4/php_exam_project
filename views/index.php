8f3bf0286f40abc0bd8e674872af6f9996779453
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-07 23:25:18 +0100

    fixed scrolling bug

--- views/index.php
+++ views/index.php
@@ -5,7 +5,7 @@ include_once __DIR__ . '/templates/header.php';
 ?>
 
 <main class="container mx-auto h-screen p-4">
-    <section class="w-100 flex justify-center content-center h-full">
+    <section class="w-100 flex justify-center content-center h-full relative">
         <div class="self-center">
             <img class="self-center" src="./public/images/anarkist_logo_light.png" alt="Anarkist logo" />
         </div>
