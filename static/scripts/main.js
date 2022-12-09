007ec1ccb2528f2ff1cec33b0776ba33f71eedf2
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-09 16:37:59 +0100

    Image input now looks nice. Better error messaging. Posting a beer is now possible. Dynamic urls to api routes

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,10 +1,13 @@
 "use strict";
 
-function formValidation(callback) {
+function formValidation(callback, url) {
+    if (!url) {
+        url = '/';
+    }
     event.preventDefault();
     const form = event.target.form;
     if (form.checkValidity()) {
-        callback(form);
+        callback(form, url);
     }
 }
 
@@ -18,7 +21,42 @@ function toggleBurger() {
     document.querySelector(".menu").classList.toggle('show');
 }
 
-async function postUser(form) {
+function toggleLabel() {
+    const select = event.target;
+    if (select.value !== '') {
+        select.classList.remove('invalid');
+        select.classList.add('valid');
+    } else {
+        select.classList.remove('valid');
+        select.classList.add('invalid');
+    };
+}
+
+function addPreviewImage() {
+    const input = event.target;
+    console.log(input.id);
+    const preview = document.querySelector(`.preview[data-input-id='#${input.id}']`);
+    console.log(preview);
+    const image = input.files[0];
+    preview.querySelector('img').src = URL.createObjectURL(image);
+    preview.classList.remove('hidden');
+}
+
+function removePreviewImage() {
+    const inputId = event.target.dataset.inputId;
+    document.querySelector(inputId).value = "";
+    const preview = document.querySelector(`.preview[data-input-id='${inputId}']`);
+    preview.querySelector('img').src = "";
+    preview.classList.add('hidden');
+
+    // Might need to create some logic regarding updating images...
+    // const inputHidden = document.querySelector(`[type='hidden'][data-input-id='${inputId}']`)
+    // if (inputHidden) {
+    //     inputHidden.value = "";
+    // }
+}
+
+async function postUser(form, url) {
     const response = await fetch('/user', {
         method: 'POST',
         body: new FormData(form)
@@ -29,22 +67,43 @@ async function postUser(form) {
         return;
     }
 
-    window.location.href="/";    
+    window.location.href=url;    
 }
 
-async function postSession(form) {
+async function postSession(form, url) {
     const response = await fetch('/login', {
         method: 'POST',
         body: new FormData(form)
     });
     if (response.status !== 200) {
         const error = await response.json();
-        form.querySelector('.hint-container').classList.remove('hidden');
-        form.querySelector('.hint').textContent = error.info;
+        form.querySelector('.error-container').classList.remove('hidden');
+        form.querySelector('.error-container span').textContent = error.info;
         return;
     }
 
-    window.location.href="/";
+    window.location.href=url;
+}
+
+async function postBeer(form, url) {
+    const isActive = form.is_active;
+    if (isActive.value === "0") {
+        form.tapwall_no.value = "0";
+    }
+
+    const response = await fetch('/beer', {
+        method: 'POST',
+        body: new FormData(form)
+    });
+    if (response.status !== 201) {
+        const error = await response.json();
+        console.log(error);
+        form.querySelector('.error-container').classList.remove('hidden');
+        form.querySelector('.error-container span').textContent = error.info;
+        return;
+    }
+
+    window.location.href=url;
 }
 
 async function deleteSession() {
