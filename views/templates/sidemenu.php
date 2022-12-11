097e501ac9a6ce35a3cb3206816ea4116a6646c7
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-11 15:15:22 +0100

    initial admin sidemenu

---
+++ views/templates/sidemenu.php
@@ -0,0 +1,55 @@
+<aside class="col-span-2 sticky top-12 max-h-screen">
+    <nav>
+        <div class="mb-4">
+            <a href="#allUsers" class="nav-link text-lg">Users</a>
+            <ul class="pl-4">
+                <li>
+                    <a href="#allUsers" class="nav-link">All users</a>
+                </li>
+                <li>
+                    <a href="#createUser" class="nav-link">Create new user</a>
+                </li>
+                <li>
+                    <a class="nav-link">Update user</a>
+                </li>
+                <li>
+                    <a class="nav-link">Delete user</a>
+                </li>
+            </ul>
+        </div>
+        <div class="mb-4">
+            <a class="nav-link text-lg">Beers</a>
+            <ul class="pl-4">
+                <li>
+                    <a class="nav-link">All beers</a>
+                </li>
+                <li>
+                    <a class="nav-link">Create new beer</a>
+                </li>
+                <li>
+                    <a class="nav-link">Update beer</a>
+                </li>
+                <li>
+                    <a class="nav-link">Delete beer</a>
+                </li>
+            </ul>
+        </div>
+        <div class="mb-4">
+            <a class="nav-link text-lg">Breweries</a>
+            <ul class="pl-4">
+                <li>
+                    <a class="nav-link">All breweries</a>
+                </li>
+                <li>
+                    <a class="nav-link">Create new brewery</a>
+                </li>
+                <li>
+                    <a class="nav-link">Update brewery</a>
+                </li>
+                <li>
+                    <a class="nav-link">Delete brewery</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</aside>
