0863ba58c757d869a11c36ec688498f297d0145e
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-11 20:33:20 +0100

    Updated sidemenu items

--- views/templates/sidemenu.php
+++ views/templates/sidemenu.php
@@ -9,11 +9,16 @@
                 <li>
                     <a href="#createUser" class="nav-link">Create new user</a>
                 </li>
+            </ul>
+        </div>
+        <div class="mb-4">
+            <a class="nav-link text-lg">Breweries</a>
+            <ul class="pl-4">
                 <li>
-                    <a class="nav-link">Update user</a>
+                    <a href="#allBreweries" class="nav-link">All breweries</a>
                 </li>
                 <li>
-                    <a class="nav-link">Delete user</a>
+                    <a href="#createBrewery" class="nav-link">Create new brewery</a>
                 </li>
             </ul>
         </div>
@@ -21,33 +26,10 @@
             <a class="nav-link text-lg">Beers</a>
             <ul class="pl-4">
                 <li>
-                    <a class="nav-link">All beers</a>
+                    <a href="#allBeers" class="nav-link">All beers</a>
                 </li>
                 <li>
-                    <a class="nav-link">Create new beer</a>
-                </li>
-                <li>
-                    <a class="nav-link">Update beer</a>
-                </li>
-                <li>
-                    <a class="nav-link">Delete beer</a>
-                </li>
-            </ul>
-        </div>
-        <div class="mb-4">
-            <a class="nav-link text-lg">Breweries</a>
-            <ul class="pl-4">
-                <li>
-                    <a class="nav-link">All breweries</a>
-                </li>
-                <li>
-                    <a class="nav-link">Create new brewery</a>
-                </li>
-                <li>
-                    <a class="nav-link">Update brewery</a>
-                </li>
-                <li>
-                    <a class="nav-link">Delete brewery</a>
+                    <a href="#createBeer" class="nav-link">Create new beer</a>
                 </li>
             </ul>
         </div>
