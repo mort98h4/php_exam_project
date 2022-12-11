d8eca831f9e87687cb8f3038a53f67fc5630bdc7
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-11 21:04:06 +0100

    Created api for getting a brewery

---
+++ apis/get_brewery.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '/../utils.php';
+require_once __DIR__ . '/../classes/DBConnection.php';
+
+$validSession = _validateSession($_SESSION);
+if (!$validSession) _respond('Unauthorized attempt.', 401);
+if (!is_numeric($brewery_id)) _respond('Invalid brewery id.', 400);
+if (!in_array($_SESSION['user_role'], ['1', '2'])) _respond('Unauthorized attempt.', 401);
+
+try {
+    $db = new DB;
+    $db = $db->connect();
+
+    $query = $db->prepare('SELECT * FROM breweries WHERE brewery_id = :brewery_id LIMIT 1');
+    $query->bindValue(':brewery_id', $brewery_id);
+    $query->execute();
+
+    $brewery = $query->fetch();
+
+    if (!$brewery) {
+        _respond('Brewery does not exist.', 400);
+    }
+
+    header('Content-Type: application/json');
+    http_response_code(200);
+    echo json_encode($brewery);
+} catch(Exception $ex) {
+    _respond($ex, 500);
+}
