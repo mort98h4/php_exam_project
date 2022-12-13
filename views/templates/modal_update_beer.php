05241fc94c190e4097beb9954fa8ccf4a2600966
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-13 17:08:29 +0100

    removed .show

--- views/templates/modal_update_beer.php
+++ views/templates/modal_update_beer.php
@@ -3,7 +3,7 @@ include_once __DIR__ . '/../../utils.php';
 $breweries = _getBreweries();
 ?>
 
-<section id="update_beer_modal" class="modal show">
+<section id="update_beer_modal" class="modal">
     <div class="h-full w-full absolute top-0 left-0 z-10" onclick="toggleUpdateModal()" data-target="#update_beer_modal"></div>
     <div class="relative z-20 w-full sm:w-3/4 lg:w-2/4 m-4 p-4 bg-black flex flex-wrap justify-between border-2 border-white shadow-2xl">
         <header class="flex items-center justify-between w-full mb-4">
