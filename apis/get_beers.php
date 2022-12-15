8f0d4b629e3d2582285be07fede2d92188b25f43
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-15 12:32:34 +0100

    Added validation, updated query, uses _respond()

--- apis/get_beers.php
+++ apis/get_beers.php
@@ -2,17 +2,26 @@
 require_once __DIR__ . '/../utils.php';
 require_once __DIR__ . '/../classes/DBConnection.php';
 
+$validSession = _validateSession($_SESSION);
+if (!$validSession) _respond('Unauthorized attempt.', 401);
+if (!is_numeric($offset)) _respond('Invalid offset.', 400);
+if (!in_array($_SESSION['user_role'], ['1', '2'])) _respond('Unauthorized attempt.', 401);
+
 try {
     $db = new DB;
     $db = $db->connect();
 
-    $query = $db->prepare('SELECT * FROM beers_and_breweries');
+    $query = $db->prepare('SELECT * FROM beers_and_breweries ORDER BY beer_created_at DESC LIMIT 2 OFFSET :offset');
+    $query->bindValue(':offset', $offset, PDO::PARAM_INT);
     $query->execute();
 
     $beers = $query->fetchAll();
-    echo json_encode($beers);
-    header('Content-Type: application/json');
-    http_response_code(200);
+
+    if (!$beers) {
+        _respond('', 204);
+    }
+
+    _respond($beers, 200);
 } catch(Exception $ex) {
     _respond($ex, 500);
 }
