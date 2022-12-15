7393414d5af0f8778c4d118d302ab23dc9863aa6
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-15 14:04:35 +0100

    Added parent container to each user article, and added btn to fetch more users

--- views/admin.php
+++ views/admin.php
@@ -15,35 +15,41 @@ $users = _getUsers();
         <h1 class="text-3xl font-medium ">Admin panel</h1>
         <section id="allUsers" class="pt-12">
             <h2 class="text-2xl font-medium mb-4">All users</h2>
-
-            <?php foreach($users as $user) { ?>
-            <article id="user_<?= out($user['user_id']) ?>" class="user">
-                <header class="flex items-center justify-between">
-                    <h3 class="text-xl font-medium mb-2"><?= out($user['user_first_name']) ?> <?= out($user['user_last_name']) ?></h3>
-                    <div>
-                        <button class="btn-icon mr-2" onclick="toggleUpdateModal()" data-target="#update_user_modal" data-table="users" data-id="<?= out($user['user_id']) ?>" >
-                            <i class="fa-sharp fa-solid fa-pen-to-square pointer-events-none"></i>
-                        </button>
-                        <button class="btn-icon text-red-600" onclick="toggleDeleteModal()" data-target="#delete_user_modal" data-id="<?= out($user['user_id']) ?>">
-                            <i class="fa-sharp fa-solid fa-trash pointer-events-none"></i>
-                        </button>
+            <div id="users" class="w-full">
+                <?php foreach($users as $user) { ?>
+                <article id="user_<?= out($user['user_id']) ?>" class="user">
+                    <header class="flex items-center justify-between">
+                        <h3 class="text-xl font-medium mb-2"><?= out($user['user_first_name']) ?> <?= out($user['user_last_name']) ?></h3>
+                        <div>
+                            <button class="btn-icon mr-2" onclick="toggleUpdateModal()" data-target="#update_user_modal" data-table="users" data-id="<?= out($user['user_id']) ?>" >
+                                <i class="fa-sharp fa-solid fa-pen-to-square pointer-events-none"></i>
+                            </button>
+                            <button class="btn-icon text-red-600" onclick="toggleDeleteModal()" data-target="#delete_user_modal" data-id="<?= out($user['user_id']) ?>">
+                                <i class="fa-sharp fa-solid fa-trash pointer-events-none"></i>
+                            </button>
+                        </div>
+                    </header>
+                    <div class="grid grid-cols-2 gap-4 items-start">
+                        <div class="col-span-1">
+                            <h4 class="font-medium opacity-70 uppercase">E-mail</h4>
+                            <p class="email">
+                                <?= out($user['user_email']) ?>
+                            </p>
+                        </div>
+                        <div class="col-span-1">
+                            <h4 class="font-medium opacity-70 uppercase">Role</h4>
+                            <p class="role">
+                                <?= out($user['role_name']) ?>
+                            </p>
+                        </div>
                     </div>
-                </header>
-                <div class="grid grid-cols-2 gap-4 items-start">
-                    <div class="col-span-1">
-                        <h4 class="font-medium opacity-70 uppercase">E-mail</h4>
-                        <p class="email">
-                            <?= out($user['user_email']) ?>
-                        </p>
-                    </div>
-                    <div class="col-span-1">
-                        <h4 class="font-medium opacity-70 uppercase">Role</h4>
-                        <p class="role">
-                            <?= out($user['role_name']) ?>
-                        </p>
-                    </div>
-                </div>
-            </article>
+                </article>
+                <?php } ?>
+            </div>
+            <?php if (count($users) == 5) { ?>
+            <div class="w-full flex justify-center">
+                <button class="btn my-4" onclick="getUsers()" data-offset="5">Load more</button>
+            </div>
             <?php } ?>
 
         </section>
