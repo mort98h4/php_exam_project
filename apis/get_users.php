eb9f3cd296f47b08fc819cbf0f9e6a6072d8adfd
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-15 14:03:33 +0100

    added validation, updated query, now uses _respond

--- apis/get_users.php
+++ apis/get_users.php
@@ -1,20 +1,27 @@
 <?php
-
+require_once __DIR__ . '/../utils.php';
 require_once __DIR__ . '/../classes/DBConnection.php';
+
+$validSession = _validateSession($_SESSION);
+if (!$validSession) _respond('Unauthorized attempt.', 401);
+if (!is_numeric($offset)) _respond('Invalid offset.', 400);
+if (intval($_SESSION['user_role']) !== 1) _respond('Unauthorized attempt.', 401); 
 
 try {
     $db = new DB;
     $db = $db->connect();
 
-    $query = $db->prepare('SELECT * FROM users');
+    $query = $db->prepare('SELECT * FROM users_and_roles ORDER BY user_id ASC LIMIT 5 OFFSET :offset');
+    $query->bindValue(':offset', $offset, PDO::PARAM_INT);
     $query->execute();
 
     $users = $query->fetchAll();
-    header('Content-Type: application/json');
-    http_response_code(200);
 
-    echo json_encode(($users));
-
+    if (!$users) {
+        _respond('', 204);
+    }
+    
+    _respond($users, 200);
 } catch(PDOException $ex) {
     http_response_code(200);
     $message = [
