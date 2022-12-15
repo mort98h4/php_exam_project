77360c48047727c64a6dfb23f94c0ce151c8145c
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-15 14:06:32 +0100

    Parsing bool to _getBreweries, and added margin to button

--- views/templates/all_breweries.php
+++ views/templates/all_breweries.php
@@ -1,5 +1,5 @@
 <?php 
-$breweries = _getBreweries();
+$breweries = _getBreweries(false);
 ?>
 
 <div id="breweries" class="w-full">
@@ -23,7 +23,7 @@ $breweries = _getBreweries();
 if (count($breweries) == 5) {
 ?>
 <div class="w-full flex justify-center">
-    <button class="btn mb-4" onclick="getBreweries()" data-offset="5">Load more</button>
+    <button class="btn my-4" onclick="getBreweries()" data-offset="5">Load more</button>
 </div>
 <?php
 } 
