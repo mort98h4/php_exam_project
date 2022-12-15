61225db0f9f047d36986a254c4e2f543adcbf429
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-16 00:00:02 +0100

    Created a bad animation on the frontpage

--- views/index.php
+++ views/index.php
@@ -5,17 +5,26 @@ include_once __DIR__ . '/templates/header.php';
 include_once __DIR__ . '/templates/nav.php';
 ?>
 
-<main class="container mx-auto h-screen p-4">
-    <section class="w-100 flex justify-center content-center h-full relative">
+<main class=" mx-auto h-screen p-4 overflow-hidden">
+    <section class="w-100 mx-auto flex justify-center content-center h-full relative max-w-screen-xl">
         <div class="self-center">
-            <img class="self-center" src="./public/images/anarkist_logo_light.png" alt="Anarkist logo" />
+            <img class="self-center animate" src="./public/images/anarkist_logo_light.png" alt="Anarkist logo" />
         </div>
-        <?php if (!$validSession) { ?>
-        <div class="h-full absolute flex gap-2">
+        <div class="h-full absolute z-10 flex gap-2">
+            <?php if (!$validSession) { ?>
+        
             <button onclick="toggleModal()" data-target="#login_modal" class="btn">Log in</button>
             <a href="/signup" class="btn">Sign up</a>
+        
+            <?php } else { ?>
+            
+            <a href="/tapwall" class="btn">Go to Tapwall</a>
+
+            <?php } ?>
         </div>
-        <?php } ?>
+        <div class="absolute bottom-0 right-[-15%] frontpage-image max-w-[40%]">
+            <img src="/public/images/Fizzy-Lime-Fusion-e1667392143872-768x454.webp" />
+        </div>
     </section>
 </main>
 
