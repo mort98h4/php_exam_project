e0d1d71f357de84304337a1351eff776e336e5c9
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-15 16:33:03 +0100

    Now using absolute paths

--- views/templates/header.php
+++ views/templates/header.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? out($title) . ' | Anarkist' : 'Anarkist'; ?></title>
-    <link rel="stylesheet" href="./static/css/app.css" />
+    <link rel="stylesheet" href="/static/css/app.css" />
     <script src="https://kit.fontawesome.com/4612253046.js" crossorigin="anonymous"></script>
-    <script src="./static/scripts/main.js" defer></script>
+    <script src="/static/scripts/main.js" defer></script>
 </head>
 <body class="bg-black">
     
--- views/templates/nav.php
+++ views/templates/nav.php
@@ -11,7 +11,7 @@ if ($validSession) {
 <nav class="fixed top-0 left-0 w-full z-50 backdrop-blur-sm h-[50px]">
     <div class="container mx-auto px-4 py-2 grid grid-rows-1 grid-cols-12 items-center">
         <a href="/" class="col-span-6 md:col-span-2 md:col-start-1 md:col-end-3 xl:col-end-2 logo">
-            <img class="" src="./public/images/anarkist_logo_light.png" alt="Anarkist logo" />
+            <img class="" src="/public/images/anarkist_logo_light.png" alt="Anarkist logo" />
         </a>
         <div id="main_menu" class="menu w-full absolute md:relative overflow-x-hidden mx-[-1rem] md:mx-0 row-span-1 row-start-2 col-span-12 md:col-start-3 md:col-end-11 md:row-start-1 xl:col-start-2 xl:col-end-12 flex justify-center items-center gap-4 h-full">
             <a class="nav-link" href="/tapwall">Tapwall</a>
@@ -25,6 +25,7 @@ if ($validSession) {
             <a class="nav-link" href="/signup">Sign up</a>
             <a class="nav-link" onclick="toggleModal()" data-target="#login_modal">Log in</a>
             <?php } else { ?>
+            <a class="nav-link" href="/profile/<?= out($_SESSION['user_id']) ?>">Profile</a>
             <a class="nav-link" data-id="<?= out($_SESSION['user_id']) ?>" onclick="deleteSession()">Log out</a>
             <?php } ?>
         </div>
