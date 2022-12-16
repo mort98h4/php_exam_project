23c92e0e4ca70087f8b6a55699f72e26add564b8
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-16 11:03:50 +0100

    Created a footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,2 +1,25 @@
+    <footer class="container mx-auto grid grid-cols-12 p-4 pt-8 md:pt-4 md:items-center">
+        <div class="col-span-12 md:col-span-4 order-3 md:order-1">
+            <img src="/public/images/PINACOLADA-e1667389912238-1536x1171.png" />
+        </div>
+        <div class="col-span-12 md:col-span-4 opacity-70 text-center order-2">
+            <h4 class="uppercase font-medium mb-2">Åbningstider</h4>
+            <p>Sunday to Thursday</p>
+            <p class="">12-24</p>
+            <p>Friday and Saturday</p>
+            <p class="mb-2">12-01</p>
+        </div>
+        <div class="col-span-12 md:col-span-4 text-center md:text-right font-medium order-1 md:order-3 mb-4 md:mb-0">
+            <div class="logo mb-4">
+                <img class="mx-auto md:mr-0" src="public/images/anarkist_logo_light.png" />
+            </div>
+            <address class="opacity-70">
+                Anarkist Bar <br />
+                Bernstorffsgade 7, <br />
+                1577, København
+            </address>
+        </div>
+    </footer>
+
 </body>
 </html>
