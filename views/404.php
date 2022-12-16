4d23c0c78823977628716cde8e26718333e63a75
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-12-16 10:43:44 +0100

    small style updates

--- views/404.php
+++ views/404.php
@@ -6,12 +6,12 @@ include_once __DIR__ . '/templates/nav.php';
 ?>
 
 <main class="container mx-auto h-screen p-4 overflow-hidden relative">
-    <section class="w-full flex justify-center flex-col items-center h-full">
+    <section class="w-full pt-12 lg:p-0 flex lg:justify-center flex-col items-center h-full">
         <h1 class="text-3xl font-medium mb-2">404 - Page not found.</h1>
         <p class="mb-4">The page you are looking for does not seem to exist.</p>
         <a href="/" class="nav-link">Go back to the frontpage?</a>
     </section>
-    <div class="absolute bottom-0 right-0 max-w-[450px]">
+    <div class="absolute w-full bottom-0 right-0 max-w-[450px]">
         <img src="/public/images/Crisp-N-Cold-e1667393115584.png" />
     </div>
 </main>
